2bb320c19289a6ea01e3c9601adbe1ec02ac9da6
Fatma Ahmed <87339260+FatmAhmedM@users.noreply.github.com>
2022-07-28 01:17:37 -0700

    add remove function

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import streamlit as st
 import sqlite3
 import pickle
 st.set_page_config(layout ="wide") 
-st.write('#Welcome to Text Emotion Analyzer')
+st.write('# Welcome to Text Emotion Analyzer')
 st.warning('You have only 10 times use, after the 10th try the result will be deleted')
 
 
@@ -16,11 +16,11 @@ st.warning('You have only 10 times use, after the 10th try the result will be de
 conn = sqlite3.connect('data.db')
 c = conn.cursor()      		
 def create_table():
-	c.execute('CREATE TABLE IF NOT EXISTS nlptable(text_to_analyze TEXT)')
+	c.execute('CREATE TABLE IF NOT EXISTS nlptable(inputxt TEXT, text_to_analyze TEXT)')
 
 
-def add_data(text_to_analyze):
-	c.execute('INSERT INTO nlptable(text_to_analyze) VALUES (?)',(text_to_analyze,))
+def add_data(inputxt,text_to_analyze):
+	c.execute('INSERT INTO nlptable(inputxt,text_to_analyze) VALUES (?,?)',(inputxt,text_to_analyze))
 	conn.commit()  
 
 def view_all_notes():
@@ -29,32 +29,42 @@ def view_all_notes():
 	return data  
 
 
-#def remove_table():
-#  c.execute("DELETE FROM nlptable WHERE text_to_analyze like 11%  ") #delete the row that begin with 11
+def remove_table():
+  c.execute("DELETE FROM nlptable")
+  conn.commit()
+  
 
-create_table()
+create_table()   
 mxlen =100
 numwords = 1000
 model = keras.models.load_model('nlp_emotion_analysis.h5')
-sentence = [st.text_input('Here the text to analyze')]
+inputxt = st.text_input('Here the text to analyze')
+sentence = [inputxt]
 tokenizer = pickle.load(open('tokenizer.pickle', 'rb'))
 sequences = tokenizer.texts_to_sequences(sentence)
 padded = pad_sequences(sequences, maxlen=mxlen, padding='post', truncating= 'post')
 prediction_index = np.argmax(model.predict(padded)[0])
-count =1 #helping us to control database rows number
-result = "{} That was a tone of {}".format(count,pre(prediction_index))
+result = " That was a tone of {}".format(pre(prediction_index))
+
 
 
 if st.button('Analyze'):  
-    add_data(result)
-    #remove_table()
-    results = view_all_notes()
-    count+=1
-    col1, col2 = st.columns(2)
+    add_data(inputxt,result)
+    results = view_all_notes() #json format
+    col1, col2,col3 = st.columns(3)
     with col1:
         for i in results:
-            st.write('##' + i[0][1:]) #this [1:] for not showing the number which help us in remove_table() function
-    with col2:	
+            st.write(i[0])
+    with col2:
+      for i in results:
+        st.write(i[1])
+
+    with col3:	
         for i, result in enumerate(results):
             i+=1
+            if i ==10: #controling num of rows in database 
+              remove_table()
+              conn = sqlite3.connect('data.db')
+              c = conn.cursor() 
+
             st.write("This is the result number {}".format(i))
